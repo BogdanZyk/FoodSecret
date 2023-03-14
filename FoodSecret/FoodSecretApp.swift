50750bd69a3d60edc250484ed3bde59409e5fd4e
BogdanZyk <zykovbogdan07@gmail.com>
2023-03-14 13:12:58 +0300

    Initial Commit

---
+++ FoodSecret/FoodSecretApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodSecretApp.swift
+//  FoodSecret
+//
+//  Created by Bogdan Zykov on 14.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct FoodSecretApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
